c7d9dad0802737788d712d95deb7dac81b91f773
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-13 21:16:56 +0100

    Add project files.

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int ProductionYear { get; set; }
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+    }
+}
